54b410958b267056aa7e38a6ea19484dcaf1a140
Guille <gnoceda@jaumebalmes.net>
2023-01-18 19:15:19 +0100

    toolbar done

--- Nadal/app/src/main/java/com/example/actnadal/MainActivity.java
+++ Nadal/app/src/main/java/com/example/actnadal/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.actnadal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button submitbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitbutton=(Button)findViewById(R.id.botonLista);
+        submitbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Tarjeta.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
